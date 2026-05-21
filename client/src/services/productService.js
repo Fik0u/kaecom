7cae3f3ffc14bc0b52ba2f3e9f4b799d914ff1af
Fikou <fikouch@gmail.com>
2026-05-21 19:37:19 +0100

    Kaecom v1.1.0

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:1999/api/products/allProds";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/products/allProds`;
 
 export const getProducts = async () => {
     return await axios.get(API_URL);
